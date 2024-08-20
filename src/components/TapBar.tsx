a93b4c5e28fda88d095abdb4002edbb39fe71893
Thonida1045 <ithonida@gmail.com>
2024-08-20 14:44:01 +0700

    TapBar

--- src/components/TapBar.tsx
+++ src/components/TapBar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../assets/logo.png';
+import { TbLanguage } from "react-icons/tb";
 
 function TapBar() {
     return (
-        <div>
-            
+        <div className='justify-between flex flex-row seld-center bg-white'>
+             <img src={logo} alt="logo page" className='w-8 h-8 rounded-full'/>
+             <div className='flex space-x-8 font-bold'>
+                <p> Home</p>
+                <p className='underline'>Tree</p>
+                <p className='underline'>About</p>
+            </div>
+            <TbLanguage className='w-8 h-8'/>
         </div>
     );
 }
