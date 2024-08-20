c5b6bf1c0295ece1ca6f0161b7dbea814476c11a
Thonida1045 <ithonida@gmail.com>
2024-08-20 23:27:07 +0700

    Search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import trees from '../assets/trees.jpg'
 import { BiSearchAlt } from "react-icons/bi";
+import Bg from "../assets/Bg.jpg"
 
 function Search() {
     const [searchTerm, setSearchTerm] = useState('');
@@ -15,15 +15,18 @@ function Search() {
 //   };
     return (
         <div className='relative w-full h-screen mt-10'>
-            <img src={trees} alt="background of search" className='object-cover h-screen sm:object-fill sm:w-full rounded-md'/>
-            <div className="flex items-center space-x-80">
-            <input
-                 type="text"
-                 value={searchTerm}
-                 onChange={handleChange}
-                 placeholder="Search..."
-                 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white bg-opacity-80 p-4 rounded-md flex items-center space-x-2 w-full max-w-md"/>
-            <BiSearchAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-xl text-gray-500"/>
+            <img src={Bg} alt="background of search" className='object-cover h-screen sm:object-fill sm:w-full rounded-md'/>
+            <div className="absolute top-20 left-1/2 transform -translate-x-1/2 w-full max-w-md">
+                <div className="relative">
+                    <input
+                        type="text"
+                        value={searchTerm}
+                        onChange={handleChange}
+                        placeholder="Search..."
+                        className="w-full bg-white bg-opacity-80 p-4 pl-10 rounded-md"
+                    />
+                    <BiSearchAlt className="absolute left-3 top-1/2 transform -translate-y-1/2 text-xl text-gray-500" />
+                </div>
             </div>
         </div>
     );
