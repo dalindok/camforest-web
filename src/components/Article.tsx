ce54e06c7a52749d4bd23b2b8b9bd1fe91063e0d
dalindok <dalindok211@gmail.com>
2024-09-01 19:03:27 +0700

    init project

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,30 +1,43 @@
-import React from 'react';
-import Cheung_Chab from '../assets/Cheung Chap.jpg';
+import React from "react";
+import Cheung_Chab from "../assets/Cheung Chap.jpg";
 
 function Article() {
-    return (
-        <div className='text-center self-center'>
-            <p className='font-bold text-3xl mt-20 underline underline-offset-8'>
-                Lastest Article</p>
-            <div className='flex flex-row m-10'>
-                <div className='flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white '>
-                    <img src={Cheung_Chab} alt="Cheung Chab" className='w-full h-auto rounded-lg ' />
-                    <p className='font-bold'>Title 1</p>
-                    <p>Description 1</p>
-                </div>
-                <div className='flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white'>
-                    <img src={Cheung_Chab} alt="Cheung Chab" className='w-full h-auto rounded-lg' />
-                    <p className='font-bold'>Title 2</p>
-                    <p>Description 2</p>
-                </div>
-                <div className='flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white'>
-                    <img src={Cheung_Chab} alt="Cheung Chab" className='w-full h-auto rounded-lg' />
-                    <p className='font-bold'>Title 3</p>
-                    <p>Description 3</p>
-                </div>
-            </div>
+  return (
+    <div className="text-center self-center">
+      <p className="font-bold text-3xl mt-20 underline underline-offset-8">
+        Lastest Article
+      </p>
+      <div className="flex flex-row m-10">
+        <div className="flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white ">
+          <img
+            src={Cheung_Chab}
+            alt="Cheung Chab"
+            className="w-full h-auto rounded-lg "
+          />
+          <p className="font-bold">Title 1</p>
+          <p>Description 1</p>
         </div>
-    );
+        <div className="flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white">
+          <img
+            src={Cheung_Chab}
+            alt="Cheung Chab"
+            className="w-full h-auto rounded-lg"
+          />
+          <p className="font-bold">Title 2</p>
+          <p>Description 2</p>
+        </div>
+        <div className="flex flex-col shadow shadow-slate-300 m-10 rounded-lg bg-white">
+          <img
+            src={Cheung_Chab}
+            alt="Cheung Chab"
+            className="w-full h-auto rounded-lg"
+          />
+          <p className="font-bold">Title 3</p>
+          <p>Description 3</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Article;
